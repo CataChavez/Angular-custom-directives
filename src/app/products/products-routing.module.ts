ea5026cef2d536a0e73339a31cf2e33b580e29d3
cataChavez <catherin.chavez@zentagroup.com>
2022-01-13 17:19:40 -0300

    routes add

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'add', component: AddComponent },
+      { path: '**', redirectTo: 'add'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
